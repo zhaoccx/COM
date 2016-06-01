32df57cc6b257881f919cb7b9884f02a5f0abd39
趙常春 <zhaoccx@hotmail.com>
2016-06-01 11:01:13 +0800

    Java Servlet Init

---
+++ JavaServletLogin/src/servlet/LoginServlet.java
@@ -0,0 +1,97 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.po.Users;
+
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3091463350951773552L;
+
+	/**
+	 * Constructor of the object.
+	 */
+	public LoginServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	@Override
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to
+	 * post.
+	 * 
+	 * @param request
+	 *            the request send by the client to the server
+	 * @param response
+	 *            the response send by the server to the client
+	 * @throws ServletException
+	 *             if an error occurred
+	 * @throws IOException
+	 *             if an error occurred
+	 */
+	@Override
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		Users u = new Users();
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		u.setUsername(username);
+		u.setPassword(password);
+		// 判断用户名和密码是否合法
+		if (u.getUsername().equals("admin") && u.getPassword().equals("admin")) {
+			response.sendRedirect(request.getContextPath() + "/login_success.jsp");
+		} else {
+			response.sendRedirect(request.getContextPath() + "/login_failure.jsp");
+		}
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException
+	 *             if an error occurs
+	 */
+	@Override
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
